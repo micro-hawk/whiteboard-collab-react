4466ddd5542500c087a98292afd057c1bf1f8d47
micro-hawk <dasvikas0071@gmail.com>
2021-01-31 14:26:01 +0530

    WhiteBoard and Canvas

--- ui/src/components/board/Board.jsx
+++ ui/src/components/board/Board.jsx
@@ -1,13 +1,65 @@
-import React from 'react';
-class Board extends React.Component{
-    constructor(props){
-        super(props);
+import React from "react";
+import "./style.css";
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    this.drawOnCan();
+  }
+  drawOnCan() {
+    // create canvas element and append it to document body
+    var canvas = document.createElement("canvas");
+    document.body.appendChild(canvas);
+
+    // some hotfixes... ( ≖_≖)
+    document.body.style.margin = 0;
+    canvas.style.position = "fixed";
+
+    // get canvas 2D context and set him correct size
+    var ctx = canvas.getContext("2d");
+    resize();
+
+    // last known position
+    var pos = { x: 0, y: 0 };
+
+    window.addEventListener("resize", resize);
+    document.addEventListener("mousemove", draw);
+    document.addEventListener("mousedown", setPosition);
+    document.addEventListener("mouseenter", setPosition);
+
+    // new position from mouse event
+    function setPosition(e) {
+      pos.x = e.clientX;
+      pos.y = e.clientY;
     }
-    render(){
-        return (
-            <h1>Hello World</h1>
-        );
+
+    // resize canvas
+    function resize() {
+      ctx.canvas.width = window.innerWidth;
+      ctx.canvas.height = window.innerHeight;
     }
+
+    function draw(e) {
+      // mouse left button must be pressed
+      if (e.buttons !== 1) return;
+
+      ctx.beginPath(); // begin
+
+      ctx.lineWidth = 5;
+      ctx.lineCap = "round";
+      ctx.strokeStyle = "black";
+
+      ctx.moveTo(pos.x, pos.y); // from
+      setPosition(e);
+      ctx.lineTo(pos.x, pos.y); // to
+
+      ctx.stroke(); // draw it!
+    }
+  }
+  render() {
+    return <canvas className="board" id="board"></canvas>;
+  }
 }
 
-export default Board ;
+export default Board;
